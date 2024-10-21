59833597982445f3939563ba3a18f96f0d2e4766
vjai25 <virendra.jai25@gmail.com>
2024-10-21 21:44:17 +0530

    cart context code added into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { CartProvider } from './context/CartContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CartProvider>
     <App />
-  </StrictMode>,
+    </CartProvider>
+  </React.StrictMode>,
 )
